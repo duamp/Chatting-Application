5b45fa88ef7f2b9ffc43f0fc84f757026a1c449b
Mp6 <matteo.pulcini19@gmail.com>
2022-01-01 20:09:07 -0500

    Default Chat App - Tutorial

---
+++ ChatApp/README.md
@@ -0,0 +1,5 @@
+Dependicies: 
+Express - web framework
+Socket.io - web sockets 
+Moment - dates and times 
+Nodemon -> dont need to restart server for updated chat 
---
+++ ChatApp/utils/messages.js
@@ -0,0 +1,11 @@
+const moment = require('moment');
+
+function formatMessage(username, textmessage){
+    return {
+        username,
+        textmessage,
+        time: moment().format("h:mm a")
+    }
+}
+
+module.exports = formatMessage;
